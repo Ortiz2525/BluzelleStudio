01ed18622a9b25bbb039b53b8e07ce6625169f5c
Monty Thibault <monty@bluzelle.com>
2018-05-15 09:55:30 -0700

    Updated Bluzelle logo

--- web/src/components/Header/Header.js
+++ web/src/components/Header/Header.js
@@ -1,19 +1,20 @@
-import logo from './logo-color.png';
+import logo from './logo.png';
 
 export const Header = () => (
     <div style={{
         textAlign: 'center',
-        marginTop: 10,
-        marginBottom: 25
+        marginTop: 10
     }}>
-        <img src={logo}/>
+        <img 
+            width="320px"
+            src={logo}/>
         <h1 style={{
             display: 'inline-block',
             fontWeight: 'bold',
             fontFamily: 'Courier New',
             color: '#32658A',
             marginLeft: 35,
-            marginBottom: -3,
+            marginBottom: 18,
             verticalAlign: 'bottom',
             fontSize: 40
         }}>Database Editor</h1>
