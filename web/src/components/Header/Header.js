330dbb591627ec0b719dcac56b9a4ccc7bbe2058
Monty Thibault <monty@bluzelle.com>
2018-06-20 12:14:09 -0700

    CRUD app more beautiful

--- web/src/components/Header/Header.js
+++ web/src/components/Header/Header.js
@@ -8,15 +8,5 @@ export const Header = () => (
         <img 
             width="320px"
             src={logo}/>
-        <h1 style={{
-            display: 'inline-block',
-            fontWeight: 'bold',
-            fontFamily: 'Courier New',
-            color: '#32658A',
-            marginLeft: 35,
-            marginBottom: 18,
-            verticalAlign: 'bottom',
-            fontSize: 40
-        }}>Database Editor</h1>
     </div>
 );
