d40ee4fb2ad4ced13eee57142e3de17f71720830
Monty Thibault <monty@bluzelle.com>
2018-08-08 15:14:29 -0700

    Removed travis_retry (not found for some reason)

--- package.sh
+++ package.sh
@@ -10,7 +10,7 @@ npm run package-mac
 cd dist/bluzelle-crud-darwin-x64
 tar czf ${PACKAGE_NAME_OSX} *
 echo ${TRAVIS_BRANCH}
-travis_retry curl -u${REPO_USER}:${REPO_PASSWORD} -T ${PACKAGE_NAME_OSX} "https://bluzelle.jfrog.io/bluzelle/OSX/${PACKAGE_NAME_OSX}"
+curl -u${REPO_USER}:${REPO_PASSWORD} -T ${PACKAGE_NAME_OSX} "https://bluzelle.jfrog.io/bluzelle/OSX/${PACKAGE_NAME_OSX}"
 cd ../..
 
 
@@ -19,4 +19,4 @@ cd ../..
 npm run package-linux
 brew install fakeroot dpkg
 npm run package-deb -- --options.version ${PROJECT_VERSION}
-travis_retry curl -u${REPO_USER}:${REPO_PASSWORD} -XPUT "https://bluzelle.jfrog.io/bluzelle/debian-local/pool/${PACKAGE_NAME_DEB};deb.distribution=all;deb.component=stable;deb.architecture=${PACKAGE_ARCHITECTURE}" -T dist/*.deb
+curl -u${REPO_USER}:${REPO_PASSWORD} -XPUT "https://bluzelle.jfrog.io/bluzelle/debian-local/pool/${PACKAGE_NAME_DEB};deb.distribution=all;deb.component=stable;deb.architecture=${PACKAGE_ARCHITECTURE}" -T dist/*.deb
