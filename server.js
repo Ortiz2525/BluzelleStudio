b846d13edafbdd6fc5d9ec284dbc96992826224f
Matt I <matt.r.ilagan@gmail.com>
2018-09-18 09:42:35 -0700

    Added express server

---
+++ server.js
@@ -0,0 +1,13 @@
+
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.static(path.resolve('../dist/generated/js')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve('../dist/generated/js', 'index.html'));
+});
+
+app.listen(port);
