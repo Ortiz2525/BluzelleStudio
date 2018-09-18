8f5206765b1e99301f558838a5dada39edad3e39
Matt I <matt.r.ilagan@gmail.com>
2018-09-18 10:17:48 -0700

    Fixed path to index.html

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(path.resolve('../dist/generated/js')));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve('../dist', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
 app.listen(port);
