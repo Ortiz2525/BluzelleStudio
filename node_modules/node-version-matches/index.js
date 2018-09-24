02ece70a0eae74c49279bae5d9e2301621f257b5
Matt I <matt.r.ilagan@gmail.com>
2018-09-24 08:57:16 -0700

    Remove the now ignored directory node_modules

--- node_modules/node-version-matches/index.js
+++
@@ -1,7 +0,0 @@
-function nodeVersionMatches (semverExpression, version) {
-  version = version || require('process').version
-  const semver = require('semver')
-  return semver.satisfies(version, semverExpression)
-}
-
-module.exports = nodeVersionMatches
