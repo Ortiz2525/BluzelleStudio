cf6a5178d8a52d4b949ed28ed80bc1c2ee572e5d
Matt I <matt.r.ilagan@gmail.com>
2018-09-24 13:26:57 -0700

    skdshfskf

--- server.js
+++ server.js
@@ -5,6 +5,21 @@ const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
+var request = require('request');
+
+getAddonConfigVars() {
+  var ops = {
+      uri: 'http://api.heroku.com/apps/exampleappdemo/config-vars',
+      method: 'GET',
+      headers: {
+          'Content-Type': 'application/vnd.heroku+json'
+      }
+  }
+  request(ops, function (error, response) {
+      console.log(error, response.body);
+      return;
+  });
+}
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
