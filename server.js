b909821339bb052e6c262956ea41a001cc54d466
Matt I <matt.r.ilagan@gmail.com>
2018-10-09 21:03:35 -0700

    skdjfh

--- server.js
+++ server.js
@@ -14,14 +14,17 @@ function getToken(req){
   var appName = req.query.app;
   var ops = {
     method: 'GET',
-    accept: 'application/vnd.heroku+json; version=3',
-    authorization: 'Bearer 440fc53d-e177-4e3c-962a-570be65ab3f2'
+    headers: {
+      "Authorization": "Bearer 440fc53d-e177-4e3c-962a-570be65ab3f2",
+      "Accept": "application/vnd.heroku+json; version=3"
+    }
+    
   };
 
   return fetchUrl('https://api.heroku.com/apps/' + appName + '/config-vars', ops, function(error, meta, body){
     console.log(body.toString());
   }); 
-  
+
 }
 
 app.use(express.static(path.join(__dirname, 'dist')));
