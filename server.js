508f550fa171a2543ad95b30ed761f141c0c8fe4
Matt I <matt.r.ilagan@gmail.com>
2018-10-09 21:35:49 -0700

    skdjfh

--- server.js
+++ server.js
@@ -30,7 +30,13 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (_req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
-  console.log(_req.toString());
+  console.log("test 1 " + res.body);
+  //var appName = _req.params.app;
+  //getToken(appName);
+});
+
+app.get('/', (_req, res) => {
+  console.log("test 2 " + res.body);
   //var appName = _req.params.app;
   //getToken(appName);
 });
