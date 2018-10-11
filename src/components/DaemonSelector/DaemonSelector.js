1b5171a65e1c24ea944215c0ce11c20e1e894ec8
Matt I <matt.r.ilagan@gmail.com>
2018-10-11 11:25:01 -0700

    sdkfh

--- src/components/DaemonSelector/DaemonSelector.js
+++ src/components/DaemonSelector/DaemonSelector.js
@@ -35,8 +35,6 @@ export default class DaemonSelector extends Component {
             }
         }
 
-        const defaultUuid = uuidv4();
-
         fetch('https://api.heroku.com/apps/' + appName + '/config-vars', requestObject)
         .then(function(response){
             return response.json();
@@ -45,7 +43,7 @@ export default class DaemonSelector extends Component {
             console.log(responseJson);
             this.refs.port.value = responseJson.BLUZELLE_PORT || "8100";
             this.refs.address.value = responseJson.BLUZELLE_ADDRESS || "127.0.0.1";
-            this.refs.uuid.value = responseJson.BLUZELLE_UUID || defaultUuid;
+            this.refs.uuid.value = responseJson.BLUZELLE_UUID || "thisisatest";
             return responseJson;
         });
 
