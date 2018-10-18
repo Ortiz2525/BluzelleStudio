d7bf16d288005da14ce24ae4d9637a162dd517dd
Matt I <matt.r.ilagan@gmail.com>
2018-10-17 17:12:05 -0700

    Updated package.json to include required dependencies.

--- server.js
+++ server.js
@@ -6,38 +6,13 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
-var request = require('request');
-var uuid = require('node-uuid');
-var fetchUrl = require("fetch").fetchUrl;
-
-function getVars(appName){
-  var ops = {
-    method: 'GET',
-    headers: {
-      "Authorization": "Bearer 440fc53d-e177-4e3c-962a-570be65ab3f2",
-      "Accept": "application/vnd.heroku+json; version=3"
-    } 
-  };
-
-  return fetchUrl('https://api.heroku.com/apps/' + appName + '/config-vars', ops, function(error, meta, body){
-    console.log(body.toString());
-  }); 
-
-}
 
 app.use(express.static("dist"));
 app.use('*', function (req, res, next) {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-app.get('/cfgvars', (_req, res) => {
-  //getVars(_req.app);
-  console.log(_req.app);
-  console.log(getVars(_req.app));
-  res.send(getVars(_req.app));
-});
 
     
 app.listen(port);
 
-//440fc53d-e177-4e3c-962a-570be65ab3f2
