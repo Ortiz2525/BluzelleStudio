5f09b1ae0c72f9fc51b098d5f32e6dc5d7ddeffc
Matt I <matt.r.ilagan@gmail.com>
2018-11-03 15:57:33 -0700

    removing previously checked in node_modules

--- node_modules/local-web-server/bin/cli.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-const nodeVersionMatches = require('node-version-matches')
-
-if (nodeVersionMatches('>=7.6.0')) {
-  require('../lib/cli-app').run()
-} else {
-  console.log('Sorry, this app requires node v7.6.0 or above. Please upgrade https://nodejs.org/en/')
-}
