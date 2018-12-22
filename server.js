c6a2d9c7e7846b4f142c97e1c2b31e48cef9b826
Matt Ilagan <matthew@bluzelle.com>
2018-12-21 23:21:12 -0800

    adding server

---
+++ server.js
@@ -0,0 +1,21 @@
+// The sole purpose for this server is to host an instance on Heroku
+
+'use strict'
+
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
+
+
+app.use(express.static("dist"));
+app.use(express.static(__dirname + '/dist'));
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+
+    
+app.listen(port);
+
