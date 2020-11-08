350a3910a91545a4c6e06be7a84884d6b66a2539
Alexander Bai <vhurryharry@gmail.com>
2020-11-08 09:51:22 -0500

    Fixed importing

--- src/components/KeyList/importCSV.js
+++ src/components/KeyList/importCSV.js
@@ -1,11 +1,9 @@
 import useBluzelle from "../../services/BluzelleService"
 import Papa from "papaparse"
-import useCRUDService from "../../services/CRUDService"
 import useData from "components/DataContext/useData"
 
 const useImportCSV = () => {
     const { getClient } = useBluzelle()
-    const { create } = useCRUDService()
     const { gasPrice, maxGas, reload } = useData()
 
     const gas_info = {
@@ -37,57 +35,62 @@ const useImportCSV = () => {
     }
 
     const importCSV = (setIsLoading, setKeys) => {
-        const createFields = async (fields) => {
-            setIsLoading(true)
+        try {
+            const createFields = async (fields) => {
+                setIsLoading(true)
 
-            const keys = await getClient().keys()
+                const keys = await getClient().keys()
 
-            setKeys(keys)
+                setKeys(keys)
 
-            let i = 0
-            while (i < fields.length) {
-                await doImport(fields.slice(i, i + 1000))
-                i += 1000
+                let i = 0
+                while (i < fields.length) {
+                    await doImport(fields.slice(i, i + 1000))
+                    i += 1000
+                }
+
+                setIsLoading(false)
+                reload()
             }
 
-            setIsLoading(false)
-            reload()
+            const input = document.createElement("input")
+
+            input.type = "file"
+
+            input.onchange = () => {
+                if (input.files.length === 0) {
+                    return
+                }
+
+                if (input.files.length > 1) {
+                    alert("Please select only one file.")
+                    return
+                }
+
+                Papa.parse(input.files[0], {
+                    complete: function (results) {
+                        console.log("Errors from CSV input", results.errors)
+                        console.log("CSV metadata", results.meta)
+
+                        const table = results.data
+
+                        const goodRows = table.filter((row) => row.length >= 2)
+
+                        const fields = goodRows.map((row) => ({
+                            key: row[0],
+                            value: row[1],
+                        }))
+
+                        createFields(fields)
+                    },
+                })
+            }
+
+            input.click()
+        } catch (ex) {
+            console.error(ex)
+            alert(ex.error ? ex.error : "Error due to bluzelle network!")
         }
-
-        const input = document.createElement("input")
-
-        input.type = "file"
-
-        input.onchange = () => {
-            if (input.files.length === 0) {
-                return
-            }
-
-            if (input.files.length > 1) {
-                alert("Please select only one file.")
-                return
-            }
-
-            Papa.parse(input.files[0], {
-                complete: function (results) {
-                    console.log("Errors from CSV input", results.errors)
-                    console.log("CSV metadata", results.meta)
-
-                    const table = results.data
-
-                    const goodRows = table.filter((row) => row.length >= 2)
-
-                    const fields = goodRows.map((row) => ({
-                        key: row[0],
-                        value: row[1],
-                    }))
-
-                    createFields(fields)
-                },
-            })
-        }
-
-        input.click()
     }
 
     return importCSV
